7549821dfd62e58c97358a29aba0522853e70b4c
Sam Roberts <sam@Sams-MacBook-Air.local>
2025-02-21 08:17:24 +0000

    add validation block to restrict instance type

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,5 +11,10 @@ variable "ami" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  default = "t2.micro"
+    validation {
+      condition     = contains(["t2.micro", "t3.micro", "t3.medium", "t3.large"], var.instance_type)
+      error_message = "Invalid instance type! Use t2.micro,, t3.micro, t3.medium, or t3.large."
+    }
 }
 
