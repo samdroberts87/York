989b37d69b3c9bb118b4e89c0380c362633f5a16
Sam Roberts <sam@Sams-MacBook-Air.local>
2025-02-22 13:28:26 +0000

    add tags to route tables

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -83,6 +83,10 @@ resource "aws_internet_gateway" "main_igw" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
 
+  tags = {
+    Name = "public-subnet-route-table"
+  }
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main_igw.id
@@ -98,7 +102,12 @@ resource "aws_route_table_association" "public_association" {
 # Private Route Table (No Internet Gateway)
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-subnet-route-table"
+  }
 }
+
 
 # Associate Private Subnet with Private Route Table
 resource "aws_route_table_association" "private_association" {
